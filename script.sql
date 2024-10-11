dd17b389b0307295230d6c1a4c30fec8ee62054c
Iggor-motta <132962044+Iggor-motta@users.noreply.github.com>
2024-10-11 10:11:22 -0300

    Add files via upload

---
+++ script.sql
@@ -0,0 +1,149 @@
+CREATE SEQUENCE orderPessoa
+AS INT
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TABLE Pessoa(
+  idPessoa INTEGER NOT NULL,
+  nome VARCHAR(255),
+  cpf VARCHAR(11) NOT NULL,
+  endereco VARCHAR(255),
+  cidade VARCHAR(255),
+  estado CHAR(2),
+  telefone VARCHAR(12),
+  email VARCHAR(255),
+  CONSTRAINT PK_Pessoa PRIMARY KEY CLUSTERED(idPessoa ASC)
+);
+
+CREATE TABLE PessoaFisica(
+  FK_Pessoa_idPessoa INTEGER NOT NULL,
+  cpf VARCHAR(11) NOT NULL,
+  CONSTRAINT PK_PessoaFisica PRIMARY KEY CLUSTERED(FK_Pessoa_idPessoa ASC),
+  CONSTRAINT FK_Pessoa_PessoaFisica FOREIGN KEY(FK_Pessoa_idPessoa) REFERENCES Pessoa(idPessoa)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE PessoaJuridica(
+  FK_Pessoa_idPessoa INTEGER NOT NULL,
+  cnpj VARCHAR(14) NOT NULL,
+  CONSTRAINT PK_PessoaJuridica PRIMARY KEY CLUSTERED(FK_Pessoa_idPessoa ASC),
+  CONSTRAINT FK_Pessoa_PessoaJuridica FOREIGN KEY(FK_Pessoa_idPessoa) REFERENCES Pessoa(idPessoa)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE Usuario(
+  idUsuario INTEGER NOT NULL IDENTITY,
+  loginName VARCHAR(255) NOT NULL,
+  senha VARCHAR(255) NOT NULL,
+  CONSTRAINT PK_Usuario PRIMARY KEY CLUSTERED(idUsuario ASC)
+);
+
+CREATE TABLE Produto(
+  idProduto INTEGER NOT NULL IDENTITY,
+  nome VARCHAR(255) NOT NULL,
+  quantidade INTEGER,
+  precoVenda DECIMAL,
+  CONSTRAINT PK_Produto PRIMARY KEY CLUSTERED(idProduto ASC)
+);
+
+CREATE TABLE Movimento(
+  idMovimento INTEGER  NOT NULL IDENTITY,
+  FK_Usuario_idUsuario INTEGER NOT NULL,
+  FK_Pessoa_idPessoa INTEGER NOT NULL,
+  FK_Produto_idProduto INTEGER NOT NULL,
+  quantidade INTEGER,
+  tipo CHAR(1),
+  precoUnitario DECIMAL,
+  CONSTRAINT PK_Movimento PRIMARY KEY CLUSTERED(idMovimento ASC),
+  CONSTRAINT FK_Usuario_Movimento FOREIGN KEY(FK_Usuario_idUsuario) REFERENCES Usuario(idUsuario)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FK_Pessoa_Movimento FOREIGN KEY(FK_Pessoa_idPessoa) REFERENCES Pessoa(idPessoa)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  CONSTRAINT FK_Produto_Movimento FOREIGN KEY(FK_Produto_idProduto) REFERENCES Produto(idProduto)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+INSERT INTO Pessoa(idPessoa,nome,endereco,cidade,estado,telefone,email)
+  VALUES (NEXT VALUE FOR orderPessoa, 'Joao','Rua A, 11','Riacho do sul','PA','1111-1111','joao@riacho.com'),
+    (NEXT VALUE FOR orderPessoa, 'JJC','Rua B, Centro','Riacho do Norte','PA','1212-1212','jjc@riacho.com');
+   
+INSERT INTO PessoaFisica(FK_Pessoa_idPessoa,cpf)
+  VALUES (1,'11111111111');
+    
+INSERT INTO PessoaJuridica(FK_Pessoa_idPessoa,cnpj)
+  VALUES (2,'22222222222222');
+    
+
+INSERT INTO Usuario(loginName,senha)
+  VALUES ('op1','op1'),
+    ('op2','op2');
+
+INSERT INTO Produto(nome,quantidade,precoVenda)
+  VALUES ('Banana',100,'5.00'),
+    ('Laranja',500,'2.00'),
+    ('Manga',800,'4.00');
+
+INSERT INTO Movimento(FK_Usuario_idUsuario,FK_Pessoa_idPessoa,FK_Produto_idProduto,quantidade,tipo,precoUnitario)
+  VALUES (1,1,1,10,'E',5.00),
+    (2,2,2,20,'S',2.00),
+    (1,3,3,30,'E',4.00);
+
+SELECT p.*, pf.cpf
+FROM Pessoa p
+INNER JOIN PessoaFisica pf ON p.idPessoa = pf.FK_Pessoa_idPessoa;
+
+SELECT p.*, pj.cnpj
+FROM Pessoa p
+INNER JOIN PessoaJuridica pj ON p.idPessoa = pj.FK_Pessoa_idPessoa;
+
+SELECT m.*, p.nome as fornecedor, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON p.idPessoa = m.FK_Pessoa_idPessoa
+INNER JOIN Produto pr ON pr.idProduto = m.FK_Produto_idProduto
+WHERE m.tipo = 'E';
+
+SELECT m.*, p.nome as comprador, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON m.FK_Pessoa_idPessoa = p.idPessoa
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S';
+
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as compras
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'E'
+GROUP BY pr.nome;
+
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as vendas
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
+
+SELECT u.*
+FROM Usuario u
+LEFT JOIN Movimento m ON u.idUsuario = m.FK_Usuario_idUsuario AND m.tipo = 'E'
+WHERE m.idMovimento IS NULL;
+
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as compras
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'E'
+GROUP BY u.loginName;
+
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as vendas
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'S'
+GROUP BY u.loginName;
+
+SELECT pr.nome, SUM(m.precoUnitario * m.quantidade) / SUM(m.quantidade) as media
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
